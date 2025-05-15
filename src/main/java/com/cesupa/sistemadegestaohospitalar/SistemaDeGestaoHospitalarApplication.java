5971a2540ad74aec46ae1748c65eadab86b4b3e6
Felipe Cruz <felp.cruz.comp@gmail.com>
2025-05-15 19:12:39 -0300

    Fist commit

---
+++ src/main/java/com/cesupa/sistemadegestaohospitalar/SistemaDeGestaoHospitalarApplication.java
@@ -0,0 +1,13 @@
+package com.cesupa.sistemadegestaohospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoHospitalarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeGestaoHospitalarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cesupa/sistemadegestaohospitalar/SistemaDeGestaoHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesupa.sistemadegestaohospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoHospitalarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
